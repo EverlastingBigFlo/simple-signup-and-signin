1535d63689cb7d9c2a66f59551825435f328f6ce
everlastingbigflo <anjorin199@gmail.com>
2024-02-23 09:53:41 +0000

    23 Feb 2024, 10:53

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/layouts')
 
-<div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+<div class="min-h-screen bg-gray-800 text-gray-900 flex justify-center">
     <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
             <div class="mt-12 flex flex-col items-center">
@@ -87,7 +87,7 @@
             </div>
         </div>
         <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            
+
             <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
                 style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
             </div>
