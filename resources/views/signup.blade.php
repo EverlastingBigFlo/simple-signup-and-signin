c7fdb314ed4c59d6bd5b5e50031b31fa4bf4454b
everlastingbigflo <anjorin199@gmail.com>
2024-02-23 10:55:34 +0000

    23 Feb 2024, 11:55

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -80,14 +80,15 @@
                                 @error('password')
                                     {{ $message }}
                                 @enderror
-                                <input
+                            </small>
+                            <input
                                 class="w-full px-8 py-4 my-5 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                type="password_confirmation" placeholder="Confirm Password" value="{{ old('password_confirmation') }}" />
+                                type="password" placeholder="Confirm Password"
+                                value="{{ old('password_confirmation') }}" />
                             <small style="color: red">
                                 @error('password_confirmation')
                                     {{ $message }}
                                 @enderror
-                            </small>
                             </small>
                             <button type="submit"
                                 class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
