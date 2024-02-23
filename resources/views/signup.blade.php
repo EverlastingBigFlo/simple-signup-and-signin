f411a06ea3badbc57254ac08cb3f48d67828ad93
everlastingbigflo <anjorin199@gmail.com>
2024-02-23 13:31:31 +0000

    23 Feb 2024, 14:31

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -31,6 +31,12 @@
                                 Sign Up with Google
                             </span>
                         </button>
+                        <small class="text">
+                            {{-- where to route to when the account is being registered to the database --}}
+                            @if (session()->has('message'))
+                                <h1>{{ session()->get('message') }}</h1>
+                            @endif
+                        </small>
 
                         <button
                             class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
