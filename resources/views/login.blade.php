8eb74dc9e3e4ba7a060de0449e9c64b7c9027b2e
everlastingbigflo <anjorin199@gmail.com>
2024-02-24 21:05:30 +0000

    24 Feb 2024, 22:05

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,8 +9,8 @@
         <!-- Email input -->
         <div class="relative mb-6">
             <input type="password" name="password" value="{{ old('password') }}" placeholder="Email address"
-                class="w-full roundedpy-[0.32rem] leading-[2.15] border-0 bg-transparent px-3  outline-none transition-all duration-200 ease-linear
-                focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200
+                class="w-full py-[0.32rem] leading-[2.15] border-0 bg-transparent px-3  outline-none transition-all duration-200 ease-linear 
+                motion-reduce:transition-none dark:text-neutral-200
                 block min-h-[auto] rounded">
             <label for="exampleFormControlInput2"
                 class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Email
