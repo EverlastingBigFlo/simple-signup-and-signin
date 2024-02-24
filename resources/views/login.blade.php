de815d19d89b8f4529d34aa47d90ef62c3450ed5
everlastingbigflo <anjorin199@gmail.com>
2024-02-24 20:58:47 +0000

    24 Feb 2024, 21:58

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,25 +4,38 @@
 
     <form class="" action="{{ route('loginCommand') }}" method="post">
         <h1>Sign in</h1>
+        
         <h6>Sign in to your account and explore a world of possibilities. Your journey begins here.</h6>
-        <div>
+         <!-- Email input -->
+         <div class="relative mb-6">
             <input type="password" name="password" value="{{ old('password') }}" placeholder="Email address"
                 class="w-full roundedpy-[0.32rem] leading-[2.15] border-0 bg-transparent px-3  outline-none transition-all duration-200 ease-linear
                 focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200
-                ">
-            <input type="email" name="email" value="{{ old('email') }}"
-                class=" block min-h-[auto] w-full rounded    "
-                id="exampleFormControlInput2" placeholder="Email address" />
+                block min-h-[auto] rounded">
             <label for="exampleFormControlInput2"
                 class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Email
                 address
             </label>
         </div>
+
+
         <input type="password" name="password" value="{{ old('password') }}" placeholder="Enter password">
         <small style="color: red">
             @error('password')
             @enderror
         </small>
+
+            <!-- Password input -->
+            <div class="relative mb-6">
+                <input type="password"
+                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] 
+                    leading-[2.15] outline-none transition-all duration-200 ease-linear 
+                    focus:placeholder:opacity-100 dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    id="exampleFormControlInput22" placeholder="Password" />
+                <label for="exampleFormControlInput22"
+                    class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Password
+                </label>
+            </div>
 
         <div class="!mt-10">
             <button type="submit"
@@ -42,20 +55,8 @@
             <!-- Right column container -->
             <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
                 <form>
-                    <!-- Email input -->
-                    <div class="relative mb-6">
-
-                    </div>
-
-                    <!-- Password input -->
-                    <div class="relative mb-6">
-                        <input type="password"
-                            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="exampleFormControlInput22" placeholder="Password" />
-                        <label for="exampleFormControlInput22"
-                            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Password
-                        </label>
-                    </div>
+                  
+                
 
                     <div class="mb-6 flex items-center justify-between">
                         <!-- Remember me checkbox -->
