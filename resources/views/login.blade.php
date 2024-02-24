3dc35610be3d9416e05924add573c11963a08a7b
everlastingbigflo <anjorin199@gmail.com>
2024-02-24 20:47:39 +0000

    24 Feb 2024, 21:47

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,7 +43,7 @@
         <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
           <form>
             <!-- Email input -->
-            <div class="relative mb-6" data-te-input-wrapper-init>
+            <div class="relative mb-6">
               <input
                 type="text"
                 class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -57,7 +57,7 @@
             </div>
   
             <!-- Password input -->
-            <div class="relative mb-6" data-te-input-wrapper-init>
+            <div class="relative mb-6">
               <input
                 type="password"
                 class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
