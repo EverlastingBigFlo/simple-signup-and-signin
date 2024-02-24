6bcf6aed7c8d8ff94132e55760703cb1a6106b30
everlastingbigflo <anjorin199@gmail.com>
2024-02-24 21:00:24 +0000

    24 Feb 2024, 22:00

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 
     <form class="" action="{{ route('loginCommand') }}" method="post">
         <h1>Sign in</h1>
-        
+
         <h6>Sign in to your account and explore a world of possibilities. Your journey begins here.</h6>
          <!-- Email input -->
          <div class="relative mb-6">
@@ -19,22 +19,26 @@
         </div>
 
 
-        <input type="password" name="password" value="{{ old('password') }}" placeholder="Enter password">
-        <small style="color: red">
-            @error('password')
-            @enderror
-        </small>
+
+   
 
             <!-- Password input -->
             <div class="relative mb-6">
-                <input type="password"
-                    class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] 
+                <input type="password" 
+                name="password" 
+                value="{{ old('password') }}"
+                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] 
                     leading-[2.15] outline-none transition-all duration-200 ease-linear 
                     focus:placeholder:opacity-100 dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="exampleFormControlInput22" placeholder="Password" />
+                id="exampleFormControlInput22" 
+                placeholder="Password" />
                 <label for="exampleFormControlInput22"
                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Password
                 </label>
+                <small style="color: red">
+                    @error('password')
+                    @enderror
+                </small>
             </div>
 
         <div class="!mt-10">
