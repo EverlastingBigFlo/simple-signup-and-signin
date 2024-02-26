6a64974282e41cc17625a5b6dbcff6bf72ff6c99
everlastingbigflo <anjorin199@gmail.com>
2024-02-26 14:41:41 +0000

    26 Feb 2024, 15:41

--- database/migrations/2024_02_26_143030_reg_token.php
+++ database/migrations/2024_02_26_143030_reg_token.php
@@ -1,31 +1,32 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class RegToken extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            // In your migration file
+        // Check if the 'token' column doesn't already exist before adding it
+        if (!Schema::hasColumn('users', 'token')) {
             Schema::table('users', function (Blueprint $table) {
-                $table->string('token')->default(''); 
+                $table->string('token')->nullable(); // You can set nullable or a default value here if needed
             });
-        });
+        }
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        // You may add logic to revert the changes made in the 'up' method if needed
     }
 };
