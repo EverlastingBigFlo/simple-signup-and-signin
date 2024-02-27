32632b2728a59ff33d6f46ffb92a7542d5230361
everlastingbigflo <anjorin199@gmail.com>
2024-02-27 10:07:10 +0000

    27 Feb 2024, 11:07

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -27,7 +27,7 @@
                             @csrf
                             <input
                                 class="w-full px-8 py-4 my-5 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                type="username" username="username" placeholder="username" value="{{ old('username') }}" />
+                                type="username" username="username" placeholder="Username" value="{{ old('username') }}" />
                             <small style="color: red">
                                 @error('username')
                                     {{ $message }}
