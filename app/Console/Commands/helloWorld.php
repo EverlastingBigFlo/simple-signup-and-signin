a5c366a7fcfd3e4151d286bb2b92645cc6de26d6
everlastingbigflo <anjorin199@gmail.com>
2024-03-04 10:06:52 +0000

    4 Mar 2024, 11:06

---
+++ app/Console/Commands/helloWorld.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class helloWorld extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:hello-world';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        info('hello world');
+        return 0;
+    }
+}
