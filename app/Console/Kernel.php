2ba0fca3a9acd314f361bb005a3b52afcbe01c36
everlastingbigflo <anjorin199@gmail.com>
2024-03-07 09:41:31 +0000

    7 Mar 2024, 10:41

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // Schedule your command to run every minute
-        $schedule->command('users:auto-delete')->everyMinute();
+        $schedule->command('users:auto:delete')->everyMinute();
     }
 
 
