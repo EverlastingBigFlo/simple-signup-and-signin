fad59e36558e3a79fffab6346d67661f8d211c10
everlastingbigflo <anjorin199@gmail.com>
2024-03-07 09:18:17 +0000

    7 Mar 2024, 10:18

--- app/Console/Commands/AutoDeleteUsers.php
+++ app/Console/Commands/AutoDeleteUsers.php
@@ -4,14 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class autoDeleteUser extends Command
+class AutoDeleteUsers extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:auto-delete-user';
+    protected $signature = 'app:auto-delete-users';
 
     /**
      * The console command description.
